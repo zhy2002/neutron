6cb64fb2150429c495c83a41a97e1fd8669483a2
john.zhang <john.zhang@rubik.com.au>
2017-03-26 12:30:50 +1100

    upgrade to webpack2.

--- neutron-examples/neutron-examples-ui/webpack.config.js
+++ neutron-examples/neutron-examples-ui/webpack.config.js
@@ -1,30 +1,26 @@
-/**
- * References
- * - https://shellmonger.com/2016/01/26/using-eslint-with-webpack/
- */
+var path = require('path');
 
-//var modelName = process.argv[2].substr(2);
+process.traceDeprecation = true;
 
 var config = {
     entry: './src/main/jsx/lodgement/main.jsx',
     output: {
-        path: './src/main/webapp/js',
+        path: path.join(__dirname, 'src/main/webapp/js'),
         filename: 'index.js'
     },
     devtool: '#inline-source-map',
     module: {
-        preLoaders: [
+        rules: [
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
-                loader: 'eslint'
-            }
-        ],
-        loaders: [
+                loader: 'eslint-loader',
+                enforce: "pre"
+            },
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
-                loader: 'babel',
+                loader: 'babel-loader',
                 query: {
                     presets: ['es2015', 'stage-0', 'react']
                 }
@@ -33,7 +29,7 @@ var config = {
         ]
     },
     resolve: {
-        extensions: ['', '.js', '.jsx']
+        extensions: ['.js', '.jsx']
     }
 };
 
