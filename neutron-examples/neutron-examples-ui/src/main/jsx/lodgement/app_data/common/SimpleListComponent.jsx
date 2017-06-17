dff9f8d219cd5277d73e46fd68d3b7fd947b1bd4
john.zhang <john.zhang@rubik.com.au>
2017-06-17 10:58:52 +1000

    bug fix.

--- neutron-examples/neutron-examples-ui/src/main/jsx/lodgement/app_data/common/SimpleListComponent.jsx
+++ neutron-examples/neutron-examples-ui/src/main/jsx/lodgement/app_data/common/SimpleListComponent.jsx
@@ -1,45 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import NeutronHoc from '../../../neutron/NeutronHoc';
 import PanelComponent from '../../../bootstrap3/PanelComponent';
 import ListNeutronComponent from '../../../bootstrap3/ListNeutronComponent';
 
-export default class SimpleListComponent extends ListNeutronComponent {
+class SimpleListComponent extends ListNeutronComponent {
 
     constructor(props) {
         super(props);
 
         this.createItem = () => {
-            this.model.createItem();
+            this.props.model.createItem();
         };
     }
 
-    renderItems() {
-        const model = this.model;
-        const result = [];
-        for (let i = 0; i < model.getItemCount(); i++) {
-            const item = model.getItem(i);
-            result.push(this.props.createItemComponent(item));
-        }
-        return result;
-    }
-
     render() {
+        const props = this.props;
+
         return (
-            <div className={`simple-list-component ${this.props.className}`}>
-                <PanelComponent title={this.props.title} onAdd={this.createItem}>
-                    {this.renderItems()}
+            <div className={`${props.componentClass} ${props.className}`}>
+                <PanelComponent title={props.title} onAdd={this.createItem}>
+                    {
+                        props.model.getChildren().map(
+                            item => this.props.createItemComponent(item)
+                        )
+                    }
                 </PanelComponent>
             </div>
         );
     }
 }
 
-SimpleListComponent.propTypes = {
-    title: PropTypes.string.isRequired,
-    createItemComponent: PropTypes.func.isRequired,
-    className: PropTypes.string
-};
-
-SimpleListComponent.defaultProps = {
-    className: ''
-};
+export default NeutronHoc(
+    SimpleListComponent,
+    undefined,
+    {
+        title: PropTypes.string.isRequired,
+        createItemComponent: PropTypes.func.isRequired,
+        className: PropTypes.string
+    },
+    {
+        className: ''
+    }
+);
