8197a16c45a70752efce9ec4b69a4ef3daf3bd8c
john.zhang <john.zhang@rubik.com.au>
2017-06-21 21:38:20 +1000

    add no record message.

--- neutron-examples/neutron-examples-ui/src/main/jsx/lodgement/app_data/company_data/CompanyOtherIncomeListComponent.jsx
+++ neutron-examples/neutron-examples-ui/src/main/jsx/lodgement/app_data/company_data/CompanyOtherIncomeListComponent.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import NeutronHoc from '../../../neutron/NeutronHoc';
+import AlertInfoComponent from '../common/AlertInfoComponent';
 import CompanyOtherIncomeComponent from './CompanyOtherIncomeComponent';
 import AddListComponent from '../common/AddListComponent';
 import MainContentComponent from '../common/MainContentComponent';
@@ -8,13 +9,20 @@ function renderItems(model) {
     const items = model.getChildren().map(
         item => <CompanyOtherIncomeComponent key={item.getUniqueId()} model={item}/>
     );
+    if (items.length === 0) {
+        items.push(
+            <AlertInfoComponent>
+                Click <em>Add</em> to create a new other income record.
+            </AlertInfoComponent>
+        );
+    }
     return items;
 }
 
 function PersonOtherIncomeListComponent(props) {
-    const model = props.model;
+    const {model, componentClass} = props;
     return (
-        <MainContentComponent className={props.componentClass}>
+        <MainContentComponent className={componentClass}>
             <AddListComponent model={model}>
                 {renderItems(model)}
             </AddListComponent>
